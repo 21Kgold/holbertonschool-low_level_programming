8f03b38c2d8a57e15971cce9666409bd0196d92f
21Kgold <claudiarpa@gmail.com>
2022-02-06 08:49:17 -0800

    printf funtion test2

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
-#include <studio.h>
+#include <stdio.h>
 /**
  * mani - printf us
  * Return: 0
  */
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n")
-    return (0)
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
