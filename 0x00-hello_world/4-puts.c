b25dae430a094134d45023202014a576ca61c6f5
21Kgold <claudiarpa@gmail.com>
2022-02-05 21:20:57 -0800

    puts funtion7

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /* puts funtion */
 int main(void)
 {
-	char str[] = "/"Programming is like building a multilingual puzzle";
+	char str[] = "Programming is like building a multilingual puzzle";
 	
 	puts(str);
 	return (0);
