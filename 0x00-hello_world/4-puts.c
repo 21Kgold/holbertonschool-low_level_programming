4dfb699a0ac140be76425e1a6c9f7b30f84378fa
21Kgold <claudiarpa@gmail.com>
2022-02-06 08:18:19 -0800

    put function 12

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main function: char and puts use
- * return value of main (0)
+ * main - char and puts use
+ * return: value of main (0)
  */
 int main(void)
 {
-	char str[] = "\"Programming is like building a multilingual puzzle";	
+	char str[] = "\"Programming is like building a multilingual puzzle\n";	
 	puts(str);
 	return (0);
 }
