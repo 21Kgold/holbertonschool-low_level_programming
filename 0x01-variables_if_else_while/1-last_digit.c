9050fb03f2674c25947112ffdb1f89d14a13e21e
21Kgold <claudiarpa@gmail.com>
2022-02-07 11:30:47 -0800

    las digit evaluation2

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,21 +9,22 @@
 int main(void)
 {
 	int n;
+	int dg;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	int dg = n % 10;
+	dg = n % 10;
 	if (dg > 5)
 	{
-		printf ("Last digit of %s is %s and is greater than 5\n", n, dg);
+		printf ("Last digit of %d is %d and is greater than 5\n", n, dg);
 	}
 	if (dg == 0)
 	{
-		printf ("Last digit of %s is %s and is 0\n", n, dg);
+		printf ("Last digit of %d is %d and is 0\n", n, dg);
 	}
 	if (dg < 6 && dg !== 0)
 	{
-		printf ("Last ddigit of %s is %s and is less than 6 and not 0\n", n, dg);
+		printf ("Last ddigit of %d is %d and is less than 6 and not 0\n", n, dg);
 	}
 	return (0);
 }
