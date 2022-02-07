dc921f48138a7318e64f3f78b07ae8ed5dd53468
21Kgold <claudiarpa@gmail.com>
2022-02-07 11:32:02 -0800

    las digit evaluation3

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ int main(void)
 	{
 		printf ("Last digit of %d is %d and is 0\n", n, dg);
 	}
-	if (dg < 6 && dg !== 0)
+	if (dg < 6 && dg != 0)
 	{
 		printf ("Last ddigit of %d is %d and is less than 6 and not 0\n", n, dg);
 	}
