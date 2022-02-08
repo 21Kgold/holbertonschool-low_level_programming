23dff86e1aac87aebaeb4a6bf7f882bb5cf405eb
21Kgold <claudiarpa@gmail.com>
2022-02-07 19:38:30 -0800

    Print numbers from 0 to 9 but dont use variables char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int dg;
+
+	for (dg = '0' ; dg <= '9' ; dg++)
+	{
+		putchar(dg);
+	}
+	putchar ('\n');
+	return (0);
+}
