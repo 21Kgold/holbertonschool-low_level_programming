245b4c3aff2373098d54ecce21c9d49ed172c482
21Kgold <claudiarpa@gmail.com>
2022-02-07 19:33:01 -0800

    Print numbers from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+	char dg;
+
+	for (dg = '0' ; dg <= '9' ; ch++)
+	{
+		putchar(dg);
+	}
+	putchar ('\n');
+	return (0);
+}
