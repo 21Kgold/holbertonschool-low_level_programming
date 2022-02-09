8cdc25b12d2a7f520a4ada8e6516b580a2549975
21Kgold <claudiarpa@gmail.com>
2022-02-08 21:59:46 -0800

    Third

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - entry point
+ * @c: char type letter
+
+ * Return: return 1 if c is lowercase, 0 otherwise
+ */
+int _islower(int c)
+{
+	if (c > 96 && c < 123)
+		return (1);
+	else
+		return (0);
+}
