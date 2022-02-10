b006a85b438cb56cd46dfc87df25973437eee82b
21Kgold <claudiarpa@gmail.com>
2022-02-09 20:32:29 -0800

    10

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * times_table - Print a matrix [0..9]x[0..9]
+ */
+void times_table(void)
+{
+	int a, b, c, d, e;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			c = a * b;
+			if (c < 10 && b != 0)
+			{
+				_putchar(44);
+				_putchar(32);
+				_putchar(32);
+			}
+			if (c < 10)
+				_putchar('0' + c);
+			if (c > 9)
+			{
+				d = c / 10;
+				e = c % 10;
+				_putchar(44);
+				_putchar(32);
+				_putchar('0' + d);
+				_putchar('0' + e);
+			}
+		}
+		_putchar('\n');
+	}
+}
