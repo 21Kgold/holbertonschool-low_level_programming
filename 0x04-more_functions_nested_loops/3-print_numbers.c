940ec7b6fdfa8f3bb3e792da18b71820a698c413
21Kgold <claudiarpa@gmail.com>
2022-02-10 18:43:30 -0800

    Third4

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,11 +2,12 @@
 
 /**
  * print_numbers - prints the numbers, from 0 to 9, followed by a new line
+ * @n: number
  * Return: void
  */
 void print_numbers(void)
 {
-	int n =48;
+	int n = 48;
 
 	while (n > 47 && n < 58)
 	{
