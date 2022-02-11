e3c950c9b871be5dbe8e373607fad33bdf44c24f
21Kgold <claudiarpa@gmail.com>
2022-02-10 20:42:31 -0800

    7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,17 +1,27 @@
 #include "main.h"
 
 /**
- * print_diagonal - prints Backslash (ASCII 92) n times, plus one Line feed (ASCII 10)
+ * print_diagonal - prints a diagonal
  * Description: when n is lower than 1, only prints a Line feed
  * @n: int
  * Return: void
  */
 void print_diagonal(int n)
 {
-	char i;
-	for (i = 1 ; i <= n ; i++)
+	int i, j;
+
+	if (n <= 0)
 	{
-		_putchar(92);
-	}
 	_putchar(10);
+	}
+	else
+		for (i = 1 ; i <= n ; i++)
+		{
+			for (j = 2 ; j <= i ; j++)
+			{
+				_putchar(32);
+			}
+		_putchar(92);
+		_putchar(10);
+		}
 }
