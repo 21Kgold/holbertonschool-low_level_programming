c2477a73e13e46387a18e737c45c1e3720a5cd08
21Kgold <claudiarpa@gmail.com>
2022-02-14 21:40:48 -0800

    Second

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* print_rev - prints a string, in reverse, followed by a new line
+* @s: char
+* Return: void
+*/
+
+void print_rev(char *s)
+{
+	int a, b, c;
+
+	for (a = 0 ; s[a] != '\0' ; a++)
+	{
+		_putchar(s[a]);
+	}
+	b = a;
+	for (c = b - 1 ; s[c] >= 0 ; c--)
+	{
+		_putchar(s[c]);
+	}
+	_putchar('\n');
+}
