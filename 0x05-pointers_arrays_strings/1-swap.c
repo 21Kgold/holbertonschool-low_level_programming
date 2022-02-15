943431e81b1a0dccd92507657fc008390e72a5a1
21Kgold <claudiarpa@gmail.com>
2022-02-14 21:10:41 -0800

    First

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* swap_int - swaps the values of two integers
+* @a: int
+* @b: int
+* Return: void
+*/
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* _strlen - returns the length of a string
+* @s: char
+* Return: int
+*/
+
+int _strlen(char *s)
+{
+	int a;
+
+	for (a = 0 ; s[a] != '\0' ; a++)
+	{
+	}
+	return (a);
+}
