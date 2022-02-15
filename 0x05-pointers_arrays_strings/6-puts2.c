190baf7d4fa4533a5777dca5f2082725a6a37098
21Kgold <claudiarpa@gmail.com>
2022-02-15 14:48:39 -0800

    sixth

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,11 +7,15 @@
 
 void puts2(char *str)
 {
-	int a;
+	int a, b;
 
-	for (a = 0 ; str[a] != '\0' ; a += 2)
+	for (a = 0 ; str[a] != '\0' ; a++)
 	{
-		_putchar(str[a]);
+	}
+	b = a;
+	for (b = 0 ; b <= a ; b+=2)
+	{
+		_putchar(str[b]);
 	}
 	_putchar('\n');
 }
