24b5779ee075b654b733888013b2c6a7577d6f55
21Kgold <claudiarpa@gmail.com>
2022-02-18 11:14:24 -0800

    8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * rot13 - encodes a string using rot13
+ * @s: char string
+ * Return: char
+ */
+char *rot13(char *s)
+{
+	int i, j;
+	char *normal = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *encoded = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+	{
+		for (j = 0 ; j < 54 ; j++)
+		{
+			if (s[i] == normal[j])
+			{
+				s[i] = encoded[j];
+			}
+		}
+	}
+	return (s);
+}
