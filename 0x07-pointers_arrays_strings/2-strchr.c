5d4ec405962eb4d40d7fdc697a803da77e5207b6
21Kgold <claudiarpa@gmail.com>
2022-02-21 20:29:38 -0800

    update

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,23 +5,18 @@
  * @c: char
  * @s: char
  * Return: a pointer to the matched character or NULL if the character is not
- * found. The terminating null byte is considered part of the string, so that
- * if c is specified as '\0', these function return a pointer to the terminator
+ * found.
  */
 char *_strchr(char *s, char c)
 {
 	unsigned int i;
 
-	for (i = 0 ; s[i] != '\0' ; i++)
+	for (i = 0 ; s[i] >= '\0' ; i++)
 	{
 		if (s[i] == c)
 		{
 			return (s + i);
 		}
-	}
-	if (c == '\0')
-	{
-		return (s + i + 1);
 	}
 	return ('\0');
 }
