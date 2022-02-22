6e32c4c90471ac76716460db339c3a04ba25194e
21Kgold <claudiarpa@gmail.com>
2022-02-22 07:31:50 -0800

    update

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strspn - calculates the length (in bytes) of the initial segment of s which
+ * consists entirely of bytes in accept.
+ * @accept: char, string containing the list of characters to match in s
+ * @s: char, string to be scanned
+ * Return: unsigned int, returns the number of bytes in the initial segment of s 
+ * which consist only of bytes from accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+	int k = 0;
+
+	for (i = 0 ; accept[i] != '\0' ; i++)
+	{
+		for (j = 0 ; s [j] != '\0' ; j++)
+		{
+			if (s[j] == accept [i])
+			{
+				k = k + 1;
+				break;
+			}
+			
+		}
+	
+	}
+	return (k + 1);
+}
