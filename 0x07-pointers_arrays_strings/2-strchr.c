82ecbfe60a7debe482a0620507f5bdda8f9671b3
21Kgold <claudiarpa@gmail.com>
2022-02-21 19:32:18 -0800

    update

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strchr - returns a pointer to the first occurrence of the character c in
+ * the string s.
+ * @c: char
+ * @s: char
+ * Return: a pointer to the matched character or NULL if the character is not
+ * found. The terminating null byte is considered part of the string, so that
+ * if c is specified as '\0', these function return a pointer to the terminator
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+	return ('\0');
+}
