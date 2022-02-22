a96aaf3655b99c09fc6874b70514d30de3644d19
21Kgold <claudiarpa@gmail.com>
2022-02-22 09:00:58 -0800

    update

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -4,31 +4,28 @@
  * consists entirely of bytes in accept.
  * @accept: char, string containing the list of characters to match in s
  * @s: char, string to be scanned
- * Return: unsigned int, returns the number of bytes in the initial segment of s 
+ * Return: unsigned int, number of bytes in the initial segment of s
  * which consist only of bytes from accept
  */
 unsigned int _strspn(char *s, char *accept)
 {
 	int i, j, k;
-	k = 0;
 
-	for (i = 0 ; accept[i] != '\0' ; i++)
+	k = 0;
+	for (i = 0 ; accept[i] >= '\0' ; i++)
 	{
-		for (j = 0 ; s [j] != '\0' ; j++)
+		for (j = 0 ; s[j] >= '\0' ; j++)
 		{
-			if (s[j] == accept [i])
+			if (s[j] == accept[i])
 			{
 				k = k + 1;
 				break;
 			}
-			if (s[j] == '\0' && accept [i] == '\0')
-			{
-				k = k + 1;
-				break;
-			}
-			
 		}
-	
+		if (s[j] == '\0' && accept[i] == '\0')
+		{
+			break;
+		}
 	}
 	return (k);
 }
