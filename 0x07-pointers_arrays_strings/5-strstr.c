28343059e83d2a5416d5b86c2bccff507403c5b4
21Kgold <claudiarpa@gmail.com>
2022-02-23 07:56:38 -0800

    update

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,23 +9,22 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	unsigned int i, j;
+	unsigned int i, j, k;
 
+	k = 0;
 	if (*needle == 0)
 	{
 		return (haystack);
 	}
-	for (j = 0 ; needle[j] ; j++)
+	for (j = 0 ; haystack[j] ; j++)
+	{
+		for (i = 1 ; needle[i] ; i++)
 		{
-		for (i = 0 ; haystack[i] ; i++)
-		{
-			if (haystack[i] == *needle)
+			if (needle[i] == haystack[j])
 			{
-				return (haystack + i);
-			}
-			else if (needle[j + 1] == '\0')
-			{
-				return ('\0');
+				k = k + 1;
+				if (k == i)
+					return (haystack + j - i);
 			}
 		}
 	}
