0202742b76bf9ee40bbc5512eb9e86d910706346
21Kgold <claudiarpa@gmail.com>
2022-03-03 20:28:27 -0800

    update

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * string_nconcat - concatenate s1 + n bytes of s2
+ * @s1: char
+ * @s2: char, If n is >= to the length of s2 then use the entire string s2
+ * @n: unsigned int
+ * Return: pointer to a newly allocated space If function fails, return NULL
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, j, size1, size2;
+	char *array;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (size1 = 0 ; s1[size1] != '\0' ; size1++)
+	for (size2 = 0 ; s2[size2] != '\0' ; size2++)
+	if (n <= size2)
+		array = malloc(sizeof(char) * (size1 + n + 1));
+	else if (n > size2)
+		array = malloc(sizeof(char) * (size1 + size2 + 1));
+	if (array == NULL)
+		return (NULL);
+	for (i = 0 ; i < size1 ; i++)
+	{
+		array[i] = s1[i];
+	}
+	if (n <= size2)
+		for (j = 0 ; j < n ; j++)
+			array[i + j] = s2[j];
+	else if (n > size2)
+		for (j = 0 ; j < size2 ; j++)
+			array[i + j] = s2[j];
+	array[i + j] = '\0';
+	return (array);
+}
