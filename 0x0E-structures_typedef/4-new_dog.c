404dcf4a6404d17f975b09bdac234aedd24e90ab
21Kgold <claudiarpa@gmail.com>
2022-03-08 16:57:33 -0800

    update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,16 +2,16 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
- * new_dog - creates a new dog
+ * new_dog - creates a new dog1
  * @name: char variable
  * @age: float variable
  * @owner: char variable
- * Return: new dog of type dog_t
+ * Return: new dog1 of type dog_t
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	int name_size, owner_size, i;
-	dog_t *dog;
+	dog_t *dog1;
 
 	for (name_size = 0 ; name[name_size] != '\0' ; ++name_size)
 	{
@@ -19,27 +19,26 @@ dog_t *new_dog(char *name, float age, char *owner)
 	for (owner_size = 0 ; owner[owner_size] != '\0' ; ++owner_size)
 	{
 	}
-	dog = malloc(sizeof(dog_t));
-	dog->name = malloc(name_size * sizeof(char));
-	dog->owner = malloc(owner_size * sizeof(char));
-	if (dog == NULL || dog->name == NULL || dog->owner == NULL)
+	dog1 = malloc(sizeof(dog_t));
+	dog1->name = malloc(name_size * sizeof(char));
+	dog1->owner = malloc(owner_size * sizeof(char));
+	if (dog1 == NULL || dog1->name == NULL || dog1->owner == NULL)
 	{
-		free(dog->owner);
-		free(dog->name);
-		free(dog);
+		free(dog1->name);
+		free(dog1);
 		return (NULL);
 	}
 	else
 	{
 		for (i = 0 ; i < name_size ; i++)
 		{
-			dog->name[i] = name[i];
+			dog1->name[i] = name[i];
 		}
-		dog->age = age;
+		dog1->age = age;
 		for (i = 0 ; i < owner_size ; i++)
 		{
-			dog->owner[i] = owner[i];
+			dog1->owner[i] = owner[i];
 		}
-		return (dog);
+		return (dog1);
 	}
 }
