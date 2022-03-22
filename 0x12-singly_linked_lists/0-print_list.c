ef33193afc94571887e0743cb0a3338478cd2277
21Kgold <claudiarpa@gmail.com>
2022-03-21 21:19:31 -0700

    update

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * print_list - Prints the elements of a single linked list
+ * @h: pointer to the first element of a single linked list
+ * Return: size_t type
+ */
+size_t print_list(const list_t *h)
+{
+	int i = 0;
+	if (h == NULL)
+	{
+		printf("[0] (nil)\n")
+	}
+	for ( ; h != NULL ; i++)
+	{
+		printf("[%d] %s\n", h->len, h->next);
+		h = h->next;
+	}
+	return (i);
+}
