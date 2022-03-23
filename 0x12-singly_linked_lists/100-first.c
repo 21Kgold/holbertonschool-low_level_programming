ccf34bba40e516291f99f9ce747c0013acd81699
21Kgold <claudiarpa@gmail.com>
2022-03-22 19:51:47 -0700

    update

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * before_main - print a string before main function, due to the constructor
+ * atribute included in the header file.
+ * Return: void
+ */
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon ");
+	printf("my back!\n");
+}
