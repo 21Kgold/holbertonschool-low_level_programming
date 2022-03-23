855c44b1aade9f81f9b9ba2aa1692f748b52876b
21Kgold <claudiarpa@gmail.com>
2022-03-22 20:11:59 -0700

    update

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_listint - frees a listint_t list
+ * @head: pointer to the first element of a single linked list
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *new = head;
+
+	for ( ; new->next || new->next == NULL ;)
+	{
+		free(new);
+		head = head->next;
+		new = head;
+	}
+}
