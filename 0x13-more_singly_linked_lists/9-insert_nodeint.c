7ce544f43e97060d2b60530ac97f0b846c39a0eb
21Kgold <claudiarpa@gmail.com>
2022-03-23 22:07:21 -0700

    update

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -34,7 +34,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			}
 			temp = temp->next;
 		}
-		if (idx <= i)
+		if (idx <= i + 1)
 			return (new);
 	}
 	return (NULL);
