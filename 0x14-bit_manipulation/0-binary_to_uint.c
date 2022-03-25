0f5b7831010d0551004e34128f8bff2caa15dec9
21Kgold <claudiarpa@gmail.com>
2022-03-25 15:21:22 -0700

    update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,18 +7,22 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int i;	
+	int i;
 	unsigned int j = 0;
 	unsigned int k = 0;
 
+	if (b == NULL)
+	{
+		return (0);
+	}
 	for (i = 0; b[i] != '\0'; i++)
 	{
-		if ((b[i] != '0' && b[i] != '1' && b[i] != '\0') || b == NULL)
+		if (b[i] != '0' && b[i] != '1' && b[i] != '\0')
 		{
 			return (0);
 		}
 	}
-	for (i = i -1 ; i >= 0; i--)
+	for (i = i - 1 ; i >= 0; i--)
 	{
 		if (b[i] == '1')
 		{
