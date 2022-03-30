8cd3e8f837e2148e3b0556efa9ce1d6cb9379142
21Kgold <claudiarpa@gmail.com>
2022-03-30 08:27:09 -0700

    update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	if (argv[1] == NULL)
