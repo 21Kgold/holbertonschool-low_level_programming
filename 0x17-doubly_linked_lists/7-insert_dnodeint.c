fcadf040a151c000bde0e2e4a961d3d515043546
21Kgold <claudiarpa@gmail.com>
2022-05-06 14:37:58 -0700

    update

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -36,7 +36,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			before->next = new;
 			current->prev = new;
 			return (new);
-		} 
+		}
 		before = current;
 		current = current->next;
 	}
